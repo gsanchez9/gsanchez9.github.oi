073d092b47fef1a6390d23bca8c8e727ead97657
Grethel <gsanchez.9@gmail.com>
2020-02-11 20:36:31 -0600

    Cambio el nombre

---
+++ main.js
@@ -0,0 +1,11 @@
+
+//let perro1 = "Pedrito"
+//let perro2 = "Rufo"
+//let perro3 = "Chancla"
+
+//console.log ("Mis mascotas son:" +perro1+" "+ perro2+ " " +perro3+" "+". Les quiero mucho");
+
+//let nombreDelaMascota = prompt ("Digite el nombre de su primer mascota ")
+//let nombreDelaSegundaMascota = prompt ("Digite el nombre de su segunda mascota")
+
+//console.log ("Quiero mucho a mis mascotas: " +nombreDelaMascota+" " +nombreDelaSegundaMascota)
